0e8ac299aa2d3dbd8aa6a30b4d1d7602f0e6eb12
Chayton Hamric <ChaytonHamric@gmail.com>
2021-03-20 15:17:27 -0400

    Basic layout set

--- app.py
+++ app.py
@@ -6,31 +6,31 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_ERI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        content = db.Column(db.String(200), nullable=False)
-        date_created = db.Column(db.DateTime, default=datetime.utcnow)
+# class Todo(db.Model):
+#         id = db.Column(db.Integer, primary_key=True)
+#         content = db.Column(db.String(200), nullable=False)
+#         date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-        def __repr__(self):
-            return '<Task %r>' % self.id
+#         def __repr__(self):
+#             return '<Task %r>' % self.id
 
 # 157.230.63.172 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+    # if request.method == 'POST':
+    #     task_content = request.form['content']
+    #     new_task = Todo(content=task_content)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue'
+    #     try:
+    #         db.session.add(new_task)
+    #         db.session.commit()
+    #         return redirect('/')
+    #     except:
+    #         return 'There was an issue'
 
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+    # else:
+    #     tasks = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
