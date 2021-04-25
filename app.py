b6d4d009c24bfa51ce5d780cedbdd5456805269e
Chayton Hamric <ChaytonHamric@gmail.com>
2021-04-25 17:51:12 -0400

    All html and css files merged into flask but not fully linked to each other

--- app.py
+++ app.py
@@ -17,6 +17,55 @@ def index():
         stock_info = request.form['content']
     return render_template('index.html')
 
+
+@app.route("/stocks", methods=['POST', 'GET'])
+def stocks():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('stocks.html')
+
+
+@app.route("/contact", methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('contact.html')
+
+@app.route("/about", methods=['POST', 'GET'])
+def about():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('about.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('login.html')
+
+
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('sign-up.html')
+
+
+@app.route("/index2", methods=['POST', 'GET'])
+def index2():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('index2.html')
+
+
+@app.route("/mypage", methods=['POST', 'GET'])
+def mypage():
+    if request.method == 'POST':
+        stock_info = request.form['content']
+    return render_template('mypage.html')
+
+
 @app.route('/plot.png')
 def plot_png():
     getvar = "postwas"
